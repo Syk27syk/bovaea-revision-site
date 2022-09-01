0fbf86012dfe07ec0c1ec752c14a81d52c8c5ddc
shiyunkhoo <shiyun.khoo@gmail.com>
2022-09-01 12:24:20 +0800

    nav

---
+++ components/Nav.vue
@@ -0,0 +1,107 @@
+<template>
+    <div>
+        <div id="BOVAEA part 1">
+            <NuxtLink to=""> Property Taxation </NuxtLink>
+            <NuxtLink to=""> Intro to Law </NuxtLink>
+            <NuxtLink to=""> Principles and Practice of Marketing </NuxtLink>
+            <NuxtLink to=""> Principles of Economics</NuxtLink>
+            <NuxtLink to=""> Building Technology Part I</NuxtLink>
+            <NuxtLink to=""> Accounting </NuxtLink>
+        </div>
+        <div id="BOVAEA part 2">
+            <NuxtLink to=""> Estate Agency Law &#40;Professional Practice&#41;</NuxtLink>
+            <NuxtLink to=""> Land Economics </NuxtLink>
+            <NuxtLink to=""> Principles of Valuation </NuxtLink>
+            <NuxtLink to=""> Laws Relating to Property </NuxtLink>
+            <NuxtLink to=""> Real Estate Agency Practice </NuxtLink>
+            <NuxtLink to=""> Building Technology Part II </NuxtLink>
+        </div>
+        <div id="Provisional Estate Agent Test of Professional Competence">
+            <p>to be completed</p>
+        </div>
+        <div id="digital marketing resources">
+            <NuxtLink to=""> Social media marketing </NuxtLink>
+            <NuxtLink to =""> Search engine optimization </NuxtLink>
+            <NuxtLink to=""> Graphic design </NuxtLink>
+        </div>
+        <div id="sales">
+            <p> basic sales cycle </p>
+            <p> negotiation for sales and marketing </p>
+            <p> persuasion skills for sales and marketing </p>
+            <p> territory management </p>
+            <p> pre-call planning and research </p>
+            <p> discovery </p>
+            <p> sales prospecting </p>
+            <p> handling objections </p>
+            <p> closing skills </p>
+            <p> post-closing </p>
+            <p> sales networks </p>
+        </div>
+        <div id="analytics for sales and marketing">
+            <p> metrics for sales and marekting </p>
+        </div>
+        <div id="real estate investment and finance">
+            <p> urban economics </p>
+            <p> applying the economist's toolkit on real estate markets </p>
+            <p> real estate cycles </p>
+            <p> pricing characteristics of properties - hedonic modelling </p>
+            <p> fundamentals of valuation finance </p>
+            <p> mortgages </p>
+            <p> fundamentals in real estate investments </p>
+            <p> sustainability in real estate </p>
+        </div>
+        <div id="real estate research">
+            <p> real estate research </p>
+        </div>
+        <div id="property development">
+            <p> property development </p>
+        </div>
+        <div id="property management">
+            <p> property management </p>
+        </div>
+        <div id="property valuation">
+            <p> property valuation </p>
+        </div>
+        <div id="real estate by category of property">
+            <p> residential </p>
+            <p> commercial </p>
+            <p> industrial </p>
+            <p> vacant land </p>
+        </div>
+        <div id="malaysian real estate">
+            <p>market outlook, data and trends"</p>
+            <div id="by category of property">
+                <p> residential </p>
+                <p> commercial </p>
+                <p> industrial </p>
+                <p> vacant land </p>
+            </div>
+            <p> investors </p>
+            <p> homebuyers </p>
+            <p> rental </p>
+            <p>resource list</p>
+        </div>
+        <div id="international real estate">
+            <p> tax issues</p>
+            <p> customs, laws and religion</p>
+            <p> currency and exchange rates </p>
+            <p> market conditions </p>
+            <p> strategies for international real estate </p>
+            <p> negotiation in international real estate </p>
+            <p> marketing in internatioanl real estate </p>
+            <p> investment performance in international real estate </p>
+            <p> geography in international real estate </p>
+            <div id="international real estate by region">
+                <p> Asia Pacific </p>
+                <p> Europe </p>
+                <p> Eastern Europe </p>
+                <p> Australiasia </p>
+                <p> North America </p>
+                <p> South America </p>
+            </div>
+        </div>
+        <div id="financial planning for real estate investment">
+            <p> financial planning </p>
+        </div>
+    </div>
+</template>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'ztm-template',
+    title: 'bovaea revision site',
     htmlAttrs: {
       lang: 'en'
     },
